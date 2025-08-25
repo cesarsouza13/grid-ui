6928b501369181c7f30b89876ec32df379c04ced
cesarsouza13 <cesardanielmg@hotmail.com>
2025-08-25 11:37:55 -0300

    servicos ususario

---
+++ src/store/AuthSlice.ts
@@ -0,0 +1,42 @@
+// src/store/authSlice.ts
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import Cookies from 'js-cookie';
+
+export type UserAuth = {
+  id: string;
+  name: string;
+};
+
+interface AuthState {
+  isAuthenticated: boolean;
+  user: UserAuth | null;
+  
+}
+
+const initialState: AuthState = {
+  isAuthenticated: !!Cookies.get('token'),
+  user: null, 
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setLogin: (state, action: PayloadAction<{ user: UserAuth}>) => {
+        state.isAuthenticated = true;
+        state.user = action.payload.user;
+      },
+    setLogout: (state) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      Cookies.remove('token'); 
+    },
+    setUser: (state, action: PayloadAction<UserAuth>) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { setLogin, setLogout, setUser } = authSlice.actions;
+export const selectAuth = (state: { auth: AuthState }) => state.auth;
+export default authSlice.reducer;
